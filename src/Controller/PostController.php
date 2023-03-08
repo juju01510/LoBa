2e8e9248d7a7b9ff56f0e6054417176e1354ea0d
Ryww0 <laurent.chaitas@gmail.com>
2023-03-08 16:41:29 +0100

    like system && request findPostByCategory()

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Commentary;
+use App\Entity\Like;
 use App\Entity\Post;
 use App\Form\CommentaryType;
+use App\Form\LikeType;
 use App\Repository\CommentaryRepository;
 use App\Repository\PostRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -12,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class PostController extends AbstractController {
     #[Route('/blog', name:'app_blog')]
@@ -33,12 +36,18 @@ class PostController extends AbstractController {
         $commentary->setPost($post);
         $form = $this->createForm(CommentaryType::class, $commentary);
 
+        $like = new Like();
+        $like->setPost($post);
+        $likeForm = $this->createForm(LikeType::class, $like);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $opinion = $form->getData();
+            $commentary = $form->getData();
 
             if ($this->getUser() != null) {
-                $opinion->setUser($this->getUser());
+                $commentary->setUser($this->getUser());
+                $commentary->setDateCreated(new DateTime('now'));
+
                 $em = $doctrine->getManager();
                 $em->persist($commentary);
                 $em->flush();
@@ -47,9 +56,25 @@ class PostController extends AbstractController {
             }
         }
 
+        $likeForm->handleRequest($request);
+        if ($likeForm->isSubmitted() && $likeForm->isValid()) {
+            $like = $likeForm->getData();
+
+            if ($this->getUser() != null) {
+                $like->setUser($this->getUser());
+
+                $em = $doctrine->getManager();
+                $em->persist($like);
+                $em->flush();
+
+                return $this->redirectToRoute('app_blog_post', ['post' => $post->getId()]);
+            }
+        }
+
         return $this->render('test.html.twig', [
-            'form' => $form->createView(),
-            'post' => $post
+            'commentaryForm' => $form->createView(),
+            'post' => $post,
+            'likeForm' => $likeForm->createView(),
         ]);
     }
 }
--- src/Form/LikeType.php
+++ src/Form/LikeType.php
@@ -2,26 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Commentary;
+use App\Entity\Like;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CommentaryType extends AbstractType
+class LikeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-            ->add('envoyer', submitType::class)
+            ->add('like', SubmitType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Commentary::class,
+            'data_class' => Like::class,
         ]);
     }
 }
