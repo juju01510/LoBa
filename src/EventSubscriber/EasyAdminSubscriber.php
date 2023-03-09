72e2d347918b0498da1a579fe47bc3e9cec926e0
juju01510 <julien.asinari@protonmail.com>
2023-03-09 17:04:57 +0100

    Password hash en cours

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -5,7 +5,6 @@ use App\Entity\Post;
 use DateTime;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Security;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
@@ -25,14 +24,4 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         $now = new DateTime('now');
         $entity->setDateCreated($now);
     }
-
-//    public function setUser(BeforeEntityPersistedEvent $event)
-//    {
-//        $entity = $event->getEntityInstance();
-//        if(!($entity instanceof Post)){
-//            return;
-//        }
-//        $user = $this->security->getUser();
-//        $entity->setUser($user);
-//    }
 }
