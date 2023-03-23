add59c9af154a6286122e84b345e900a0ed22ae7
juju01510 <julien.asinari@protonmail.com>
2023-03-23 17:15:30 +0100

    mailtrap reçoie le mail !!!!!

--- src/Controller/NewsletterUserController.php
+++ src/Controller/NewsletterUserController.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use App\Entity\Newsletter\Users;
 use App\Form\NewsletterUserType;
 use App\Repository\Newsletter\UserRepository;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsletterUserController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/newsletter', name: 'app_newsletter_index')]
-    public function index(Request $request, UserRepository $userRepository): Response
+    public function index(Request $request, UserRepository $userRepository, MailerInterface $mailer): Response
     {
         $user = new Users();
 
@@ -26,6 +33,16 @@ class NewsletterUserController extends AbstractController
             $user->setValidationToken($token);
 
             $userRepository->save($user, true);
+
+            $email = (new TemplatedEmail())
+                ->from('julien.asinari@live.fr')
+                ->to($user->getEmail())
+                ->subject('Votre inscription à la newsletter')
+                ->htmlTemplate('email/inscription.html.twig')
+//                ->context(compact('user', 'token'))
+            ;
+
+            $mailer->send($email);
 
             $this->addFlash('message', 'Validation email sent successfully !');
             return $this->redirectToRoute('app_home');
