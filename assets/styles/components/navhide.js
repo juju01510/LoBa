243ce696ed1ab1c8968ea12d760e1b5d670e7847
DavyDesign <davydesign@hotmail.fr>
2023-03-27 12:49:42 +0200

    CSS mais pas le bon main

--- assets/styles/components/navhide.js
+++ assets/styles/components/navhide.js
@@ -1,11 +1,11 @@
-let lastScrollTop = 0;
-navbar = document.getElementById("navbar");
-window.addEventListener("scroll", function () {
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop){
-        navbar.style.top="-250px";
-    } else {
-        navbar.style.top="0";
-    }
-    lastScrollTop = scrollTop;
-})
+// let lastScrollTop = 0;
+// navbar = document.getElementById("navbar");
+// window.addEventListener("scroll", function () {
+//     const scrollTop = window.scrollY || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop){
+//         navbar.style.top="-250px";
+//     } else {
+//         navbar.style.top="0";
+//     }
+//     lastScrollTop = scrollTop;
+// })
